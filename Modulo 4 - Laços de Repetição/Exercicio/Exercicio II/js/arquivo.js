a05197646539b9212f7b4782c3fd3aa209a4cb0c
Felippe Marques <ambrsp@hotmail.com>
2023-02-02 22:09:13 -0300

    Criando elementos por js

--- Modulo 4 - Laços de Repetição/Exercicio/Exercicio II/js/arquivo.js
+++ Modulo 4 - Laços de Repetição/Exercicio/Exercicio II/js/arquivo.js
@@ -2,7 +2,27 @@
 
 document.getElementById("btn").addEventListener("click", principal);
 
+function tabuada(){
+    var num = Number(document.querySelector("#tabuada").value);
+    var tab = "";
 
+    var select = document.createElement("select");
+    select.setAttribute("id", "select-info");
+
+    var option = document.createElement("option");
+
+   
+
+
+    for(let i=0;i<=10;i++){
+        tab+= `${num} x ${i} = ${num*i} <br>`;
+        
+        
+    }
+    // select.appendChild(option);
+    document.querySelector("#resultado").innerHTML = tab;
+    
+}
 
 function validacao() {
     let campo = document.querySelector("#tabuada");
@@ -10,6 +30,7 @@ function validacao() {
     if (campo.value.length == 0) {
         campo.classList.add("error");
         campo.focus();
+        return false;
     } else {
         // a condição abaixo verifica se existe a classe error. Como se trata de uma class, que pode retornar diversos valors, tempos que fazer a contagem. Se for maior que zero, significa que ela foi ativada anteriormente
         if (document.getElementsByClassName("error").length > 0) {
