eb66caf9093b9cb9130909bdb466d9671e7d91e3
Felippe Marques <ambrsp@hotmail.com>
2023-02-06 23:53:22 -0300

    Introdução ao Javascript : Estrutura de Dados Objeto

--- Modulo 5 - Variáveis Compostas (Array)/Aula 03 - Objeto/js/arquivo.js
+++ Modulo 5 - Variáveis Compostas (Array)/Aula 03 - Objeto/js/arquivo.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", principal);
 /*
     Um objeto em programação é uma estrutura de dados que representa uma entidade do mundo real. Ele é composto por atributos, que descrevem as características da entidade, métodos, que são funções que realizam ações relacionadas à entidade, e valores, que são os dados armazenados nas propriedades.
 
-
+    O typeof de um objeto é objeto. Curiosamente, em JS, o typeof de um array tb é um objeto
 */
 
 function principal(){
