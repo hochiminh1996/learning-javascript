ee36b649ad911d34ecdbbe670aaa07929fa537fb
Felippe Marques <ambrsp@hotmail.com>
2023-02-06 23:52:01 -0300

    Introdução ao Javascript : Estrutura de Dados Objeto

---
+++ Modulo 5 - Variáveis Compostas (Array)/Aula 03 - Objeto/js/arquivo.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", principal);
+
+/*
+    Um objeto em programação é uma estrutura de dados que representa uma entidade do mundo real. Ele é composto por atributos, que descrevem as características da entidade, métodos, que são funções que realizam ações relacionadas à entidade, e valores, que são os dados armazenados nas propriedades.
+
+
+*/
+
+function principal(){
+    
+    let pessoa = { nome : "Felippe", idade : 27, estudar(nome){
+        // observe que estamos definido uma função dentro do obj, que agora é denominado de  método
+        return `${nome} está estudando. Sua idade é ${this.idade}`;
+            // this está fazendo referência ao atributo do objeto pessoa
+
+
+
+    }}
+
+
+    alert(pessoa.estudar("Felippe"));
+
+}
+
